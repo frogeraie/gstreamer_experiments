b61ae1e11c3956a21002d65d0327e872d0edb265
Roman Gaufman <hackeron@gmail.com>
2011-07-15 06:01:19 +0100

    Ruby script to test motioncells

--- ruby/gst_test.rb
+++ ruby/gst_test.rb
@@ -1,22 +1,13 @@
 require 'gst'
 
-pipeline = Gst::Pipeline.new
-
-# Source
-source = Gst::ElementFactory.make("rtmpsrc")
-source.location = 'rtmp://cdn-sov-2.musicradio.com:80/LiveAudio/LBC973'
-
-# Sink
-a = Time.new
-sink = Gst::ElementFactory.make("filesink")
-sink.location = "%04i%02i%02i_%02i%02i_lbc.mp3" % [
-  a.year, a.month, a.day, a.hour, a.min ]
-
-# Add element objects to pipeline
-pipeline.add(source, sink)
-
-# Link elements
-source >> sink
+path = File.expand_path "~/Dropbox/CCTV/1856071.mp4"
+src = "uridecodebin uri=rtsp://hackeron:password@192.168.0.252/nphMpeg4/g726-640x480 name=dec"
+# src = "filesrc location=%s ! decodebin2" % path
+#
+pipeline = Gst::Parse.launch('
+   %s ! videorate tolerance=30000000 ! videoscale ! ffmpegcolorspace ! motioncells postallmotion=true threshold=0.05 gridx=32 gridy=32 ! ffmpegcolorspace ! ximagesink' % [ src ] )
+   # %s ! videorate ! videoscale ! ffmpegcolorspace ! ximagesink' % [ src ] )
+   # %s ! videorate ! videoscale ! ffmpegcolorspace ! "video/x-raw-yuv,width=320,height=240,framerate=10/1" ! ximagesink' % [ src ] )
 
 # Main loop
 loop = GLib::MainLoop.new(nil, false)
@@ -42,13 +33,39 @@ bus.add_watch do |bus, message|
     pipeline.stop
     exit 0
   when Gst::Message::ERROR
-    puts message.structure["debug"]
+    p message.structure["debug"]
     pipeline.stop
     exit 1
+  when Gst::Message::STATE_CHANGED
+    p "%s: from: %s to %s (pending: %s)" % [
+      message.type.name,
+      message.structure["old-state"].name.sub("GST_STATE_",""),
+      message.structure["new-state"].name.sub("GST_STATE_",""),
+      message.structure["pending-state"].name.sub("GST_STATE_","") ]
+  when Gst::Message::STREAM_STATUS
+    p "%s: %s %s" % [
+      message.type.name,  message.structure["owner"].name,
+      message.structure["object"].name ]
+  when Gst::Message::TAG
+    entries_str = ''
+    message.structure.entries.each do |a,b|
+      entries_str += "%s=%s, " % [ a,b ]
+    end
+    p "%s: %s" % [ message.type.name, entries_str[0..-3] ]
+  when Gst::Message::ASYNC_DONE
+    p "%s %s" % [ message.type.name, message.structure ]
+  when Gst::Message::NEW_CLOCK
+    p "%s: %s" % [ message.type.name, message.structure["clock"].name ]
+  when Gst::Message::ELEMENT
+    p message.structure
+  when Gst::Message::QOS
+    # Used in ximagesink - nothing interesting there for us
   else
-    true
-    # puts message.type.inspect
-    # puts message.structure
+    p "--"
+    p message.type.name
+    p message.type
+    p message.structure
+    p "--"
   end
   true
 end
